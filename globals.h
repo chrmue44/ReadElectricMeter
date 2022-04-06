062561862e2ccf3a9a9d24b9e0c1354887bc3810
chrmue44 <chrmue44@users.noreply.github.com>
2022-04-06 21:23:49 +0200

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,12 +1,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#define LED_BUILTIN  2
-#define PIN_OLED_SDA 18
-#define PIN_OLED_SCL 19
-#define PIN_RX2 16
-#define PIN_TX2 17
-#define PIN_BUTTON 13
+#define LED_BUILTIN  2   // pin to drive the onboard LED
+#define PIN_OLED_SDA 18  // SDA I2C interface OLED display
+#define PIN_OLED_SCL 19  // SCL I2C interface OLED display
+#define PIN_RX2 16       // receive signal from infrared reading device
+#define PIN_TX2 17       // transmit signal to  infrared reading device
+#define PIN_BUTTON 13    // button to switch between display modes
 
 #endif //#ifndef GLOBALS_H
 
